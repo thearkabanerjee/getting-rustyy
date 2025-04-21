ef0863e199d073660928c77390046f8adf41daca
Arka Banerjee <arkabanerjee381@gmail.com>
2025-04-21 15:16:02 +0530

    printing integers

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 fn main() {
-    let i:i32 = 10;
-    println! ("{}", i);
-    // a = "hello world";
+    let i:i32 = 10; // signed number = number can be negative positive or zero
+    
+    let a :u32 = 22; // unsigned number = number that is very sure positive
+    
+    let f : f32 = 10.01; // float number = number that is a float
+    
+    print!("{}\n",i);
    
 }
