47594cfdb7c34f8aa138abced2fc3b07befbe65c
Arka Banerjee <arkabanerjee381@gmail.com>
2025-04-27 12:05:42 +0530

    rustyy

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 fn main() {
     let i:i32 = 10; // signed number = number can be negative positive or zero
     
-    let a :u32 = 22; // unsigned number = number that is very sure positive
+    let _a :u32 = 22; // unsigned number = number that is very sure positive
     
-    let f : f32 = 10.01; // float number = number that is a float
+    let _f : f32 = 10.01; // float number = number that is a float
     
     print!("{}\n",i);
    
